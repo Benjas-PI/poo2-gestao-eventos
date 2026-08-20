17a2f5bd47d7522c4eab9169a4a07547b7397843
Gabriel <gabriel.c.silva2@outlook.com>
2026-08-20 12:59:02 -0300

    FEAT: classe incricao complementada

--- src/main/java/br/ueg/eventos/domain/model/Inscricao.java
+++ src/main/java/br/ueg/eventos/domain/model/Inscricao.java
@@ -28,9 +28,31 @@ public class Inscricao {
     this.participante = participante;
     this.evento = evento;
     this.dataHoraRegistro = LocalDateTime.now();
-
     this.situacao = StatusInscricao.PENDENTE; 
     this.atividadesSelecionadas = new ArrayList<>();
+  }
+
+  public List<Atividade> getAtividadesSelecionadas() {
+    return Collections.unmodifiableList(this.atividadesSelecionadas);
+  }
+
+  public void cancelar() {
+    if (this.situacao == StatusInscricao.CANCELADA) {
+      throw new DomainRuleException("Esta inscrição já se encontra cancelada.");
+    }
+    this.situacao = StatusInscricao.CANCELADA;
+  }
+
+  public void confirmar() {
+    this.situacao = StatusInscricao.CONFIRMADA;
+  }
+
+  public void adicionarAtividade(Atividade novaAtividade) {
+    Validador.avaliar(new RegraObjetoNaoNulo(novaAtividade, "A atividade é obrigatória."));
+    
+    // TODO: tenho que fazer uma checagem de horarios em conflito aqui, vai ficar pra depois tho
+
+    this.atividadesSelecionadas.add(novaAtividade);
   }
 
   public List<Atividade> getAtividadesSelecionadas() {
